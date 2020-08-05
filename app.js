bc5d15c6622b1a97b6b9297543650da5fdeed70a
aryawardanna <aryamadridfc@gmail.com>
2020-08-06 04:55:12 +0800

    deploy heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 // import mongoose
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/db_staycation', {
+mongoose.connect('mongodb+srv://staycation-bwamern:bwamern@cluster0.atlgh.mongodb.net/db_staycation?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
